dff403c06565dde9ee5ffebbab08c1f857028667
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-13 01:42:00 -0300

    2do commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+/*Importamos React, el archivo CSS (App.css) y el componente Students. */
 import "./App.css";
 import Students from "./components/Students";
 
+/*App es un componente funcional con TypeScript (React.FC indica que es un componente funcional). */
 const App: React.FC = () => {
+  /*En el renderizado, creamos un elemento div que contiene un encabezado <h2> con el texto "Our students are:". */
+  /*Luego, utilizamos dos instancias del componente Students, proporcionando diferentes nombres ("Juan" y "Alexis") a través de la propiedad name. */
   return (
     <div>
       <h2>Our students are: </h2>
--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -1,13 +1,17 @@
-import { FC } from 'react';
+import { FC } from "react";
 
+/*Definimos una interfaz llamada StudentsProps que especifica que el componente espera recibir una propiedad llamada name de tipo string. */
 interface StudentsProps {
   name: string;
 }
 
+/*
+Luego, creamos el componente Students que recibe un objeto destructurado de las propiedades y extrae name.
+*/
+//FC: FuncionComponent
 const Students: FC<StudentsProps> = ({ name }) => {
-  return (
-    <p>I am {name}</p>
-  );
-}
+  /*Dentro del componente, mostramos un párrafo que incluye el nombre del estudiante. */
+    return <p>I am {name}</p>;
+};
 
 export default Students;
