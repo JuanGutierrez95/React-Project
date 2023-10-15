86fd9fb493164dbf3691458c69b73b1b8c2a70f6
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-14 23:18:33 -0300

    se agregó interfaces de student

---
+++ src/interfaces/Student.ts
@@ -0,0 +1,4 @@
+/*Definimos una interfaz llamada StudentsProps que especifica que el componente espera recibir una propiedad llamada name de tipo string. */
+export default interface StudentsProps {
+    name: string;
+  }
